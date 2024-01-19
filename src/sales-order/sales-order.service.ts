ce376df0f39f4fd1a3e6ef8d9eaf156b59cad770
Andres Felipe Hurtado Gil <ahurtado@heinsohn.com.co>
2024-01-19 16:36:02 +0000

    se agrega destination

--- src/sales-order/sales-order.service.ts
+++ src/sales-order/sales-order.service.ts
@@ -5,90 +5,13 @@ const { salesOrderApi, salesOrderItemApi } = salesOrderService();
 
 @Injectable()
 export class SalesOrderService {
-  // async getAllSalesOrder(): Promise<SalesOrder[]> {
-  //   return await salesOrderApi
-  //     .requestBuilder()
-  //     .getAll()
-  //     .top(10)
-  //     .addCustomHeaders({ Accept: 'application/json' })
-  //     .execute({
-  //       url: 'https://my405807-api.s4hana.cloud.sap',
-  //       username: 'USER_ADMINISTRATOR_HBT',
-  //       password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-  //     });
-  // }
+  
+  destination = {
+    url: 'https://my405807-api.s4hana.cloud.sap',
+    username: 'USER_ADMINISTRATOR_HBT',
+    password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
+  };
 
-  // async getSalesOrderById(id: string): Promise<SalesOrder> {
-  //   return await salesOrderApi
-  //     .requestBuilder()
-  //     .getByKey(id)
-  //     .addCustomHeaders({ Accept: 'application/json' })
-  //     .execute({
-  //       url: 'https://my405807-api.s4hana.cloud.sap',
-  //       username: 'USER_ADMINISTRATOR_HBT',
-  //       password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-  //     });
-  // }
-
-  // async createSalesOrder(salesOrder: Record<string, any>) {
-  //   const salesOrderItemsArray = salesOrder.to_Item.results;
-
-  //   const salerOrderItems = salesOrderItemsArray.map((item) => {
-  //     return salesOrderItemApi.entityBuilder().fromJson(item);
-  //   });
-  //   const salesOrderEntiy = salesOrderApi
-  //     .entityBuilder()
-  //     .toItem(salerOrderItems)
-  //     .fromJson(salesOrder);
-
-  //   return await salesOrderApi
-  //     .requestBuilder()
-  //     .create(salesOrderEntiy)
-  //     .addCustomHeaders({ Accept: 'application/json' })
-  //     .execute({
-  //       url: 'https://my405807-api.s4hana.cloud.sap',
-  //       username: 'USER_ADMINISTRATOR_HBT',
-  //       password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-  //     });
-  // }
-
-  // async deleteSalesOrder(salesOrderId): Promise<void> {
-  //   const response = await salesOrderApi
-  //     .requestBuilder()
-  //     .getByKey(salesOrderId)
-  //     .addCustomHeaders({ Accept: 'application/json' })
-  //     .execute({
-  //       url: 'https://my405807-api.s4hana.cloud.sap',
-  //       username: 'USER_ADMINISTRATOR_HBT',
-  //       password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-  //     });
-
-  //   return await salesOrderApi.requestBuilder().delete(response).execute({
-  //     url: 'https://my405807-api.s4hana.cloud.sap',
-  //     username: 'USER_ADMINISTRATOR_HBT',
-  //     password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-  //   });
-  // }
-
-  // async updateSalesOrder(requestBody, salesOrderId): Promise<SalesOrder> {
-  //   let response = await salesOrderApi
-  //     .requestBuilder()
-  //     .getByKey(salesOrderId)
-  //     .addCustomHeaders({ Accept: 'application/json' })
-  //     .execute({
-  //       url: 'https://my405807-api.s4hana.cloud.sap',
-  //       username: 'USER_ADMINISTRATOR_HBT',
-  //       password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-  //     });
-
-  //   Object.assign(response, requestBody);
-
-  //   return await salesOrderApi.requestBuilder().update(response).execute({
-  //     url: 'https://my405807-api.s4hana.cloud.sap',
-  //     username: 'USER_ADMINISTRATOR_HBT',
-  //     password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-  //   });
-  // }
   async getAllSalesOrder(): Promise<SalesOrder[]> {
     try {
       return await salesOrderApi
@@ -96,11 +19,7 @@ export class SalesOrderService {
         .getAll()
         .top(10)
         .addCustomHeaders({ Accept: 'application/json' })
-        .execute({
-          url: 'https://my405807-api.s4hana.cloud.sap',
-          username: 'USER_ADMINISTRATOR_HBT',
-          password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-        });
+        .execute(this.destination);
     } catch (error) {
       console.error('Error en getAllSalesOrder:', error);
       throw error; 
@@ -113,11 +32,7 @@ export class SalesOrderService {
         .requestBuilder()
         .getByKey(id)
         .addCustomHeaders({ Accept: 'application/json' })
-        .execute({
-          url: 'https://my405807-api.s4hana.cloud.sap',
-          username: 'USER_ADMINISTRATOR_HBT',
-          password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-        });
+        .execute(this.destination);
     } catch (error) {
       console.error('Error en getSalesOrderById:', error);
       throw error;
@@ -140,11 +55,7 @@ export class SalesOrderService {
         .requestBuilder()
         .create(salesOrderEntity)
         .addCustomHeaders({ Accept: 'application/json' })
-        .execute({
-          url: 'https://my405807-api.s4hana.cloud.sap',
-          username: 'USER_ADMINISTRATOR_HBT',
-          password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-        });
+        .execute(this.destination);
     } catch (error) {
       console.error('Error en createSalesOrder:', error);
       throw error;
@@ -157,17 +68,9 @@ export class SalesOrderService {
         .requestBuilder()
         .getByKey(salesOrderId)
         .addCustomHeaders({ Accept: 'application/json' })
-        .execute({
-          url: 'https://my405807-api.s4hana.cloud.sap',
-          username: 'USER_ADMINISTRATOR_HBT',
-          password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-        });
+        .execute(this.destination);
   
-      return await salesOrderApi.requestBuilder().delete(response).execute({
-        url: 'https://my405807-api.s4hana.cloud.sap',
-        username: 'USER_ADMINISTRATOR_HBT',
-        password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-      });
+      return await salesOrderApi.requestBuilder().delete(response).execute(this.destination);
     } catch (error) {
       console.error('Error en deleteSalesOrder:', error);
       throw error;
@@ -180,19 +83,11 @@ export class SalesOrderService {
         .requestBuilder()
         .getByKey(salesOrderId)
         .addCustomHeaders({ Accept: 'application/json' })
-        .execute({
-          url: 'https://my405807-api.s4hana.cloud.sap',
-          username: 'USER_ADMINISTRATOR_HBT',
-          password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-        });
+        .execute(this.destination);
   
       Object.assign(response, requestBody);
   
-      return await salesOrderApi.requestBuilder().update(response).execute({
-        url: 'https://my405807-api.s4hana.cloud.sap',
-        username: 'USER_ADMINISTRATOR_HBT',
-        password: 'AHyGnbty8neBGTVtGtbgJmpyoV#VtibskwjUTUou',
-      });
+      return await salesOrderApi.requestBuilder().update(response).execute(this.destination);
     } catch (error) {
       console.error('Error en updateSalesOrder:', error);
       throw error;
